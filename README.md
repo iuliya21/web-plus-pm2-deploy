3339f204f3ef436a07608f8f7edfb0867d73936e
iuliya21 <iuliya21@mail.ru>
2024-01-20 23:06:06 +0300

    Подготовка перед ревью

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Деплой приложения на сервер с использованием pm2
 
 Стартеркит проекта по автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
+
+IP адрес 84.201.135.108
+Frontend https://mesto-iuliya21.nomoredomainsmonster.ru/
+Backend https://api.mesto-iuliya21.nomoredomainsmonster.ru/
--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,14 +1,16 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./.env.deploy" });
+const dotenv = require('dotenv');
 
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY } =
-  process.env;
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY,
+} = process.env;
 
 module.exports = {
   apps: [
     {
-      name: "mesto",
-      script: "dist/app.js",
+      name: 'mesto',
+      script: 'dist/app.js',
     },
   ],
   deploy: {
@@ -18,8 +20,9 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      "pre-deploy-local": `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      "post-deploy": "cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy':
+        'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
